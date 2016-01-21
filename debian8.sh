0dc822af1ae44b02ac74b74c1d6a132d716a6646
antonchernik <anton@chernik.info>
2016-01-21 23:46:18 +0200

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -30,9 +30,9 @@ modprobe kvm_intel
 
 
 
-#echo "deb http://download.proxmox.com/debian jessie pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-#wget -O- "http://download.proxmox.com/debian/key.asc" | apt-key add -
-#apt-get update && apt-get dist-upgrade
-#apt-get install proxmox-ve ntp ssh postfix ksm-control-daemon open-iscsi
-#apt-get remove linux-image-amd64 linux-image-3.16.0-4-amd64 linux-base
-#update-grub
+echo "deb http://download.proxmox.com/debian jessie pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+wget -O- "http://download.proxmox.com/debian/key.asc" | apt-key add -
+apt-get update && apt-get dist-upgrade
+apt-get install proxmox-ve ntp ssh postfix ksm-control-daemon open-iscsi
+apt-get remove linux-image-amd64 linux-image-3.16.0-4-amd64 linux-base
+update-grub
