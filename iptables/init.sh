2e19758d69e456efb5b2d5c3a7f70d07a5811f76
antonchernik <anton@chernik.info>
2016-01-24 16:35:18 +0200

    Update init.sh

--- iptables/init.sh
+++ iptables/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ipset -N china hash:net
 for i in $(cat /opt/proxmox-debian/ipset/cn.zone ); do ipset -A china $i; done
-ipset -N blacklist hash:net
+ipset -N blacklist iphash
 for i in $(cat /opt/proxmox-debian/ipset/ipset-blacklist.txt ); do ipset -A blacklist $i; done
 /sbin/iptables-restore < /opt/proxmox-debian/iptables/iptables.up.rules
